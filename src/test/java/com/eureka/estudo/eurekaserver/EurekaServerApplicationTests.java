529051e3e335fc8f9eadcd3292aac739e616f1e3
Marcus V.G.Vieira <mvgv1989@gmail.com>
2019-12-13 13:33:54 -0300

    Setup do app

---
+++ src/test/java/com/eureka/estudo/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.estudo.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
